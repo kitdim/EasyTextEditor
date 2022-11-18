aed7aa887ce178640bc713dd80b24874360d701c
kitdim <kitaykin9808@gmail.com>
2022-11-18 21:36:59 +0300

    Add project files.

---
+++ LearnWPF/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LearnWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(string widnowTitle, int height, int width)
+        {
+            InitializeComponent();
+            this.Closed += MainWindow_Closed;
+            this.Closing += MainWindow_Closing;
+            this.MouseMove += MainWindow_MouseMove;
+        }
+
+        private void MainWindow_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.Title = e.GetPosition(this).ToString();
+        }
+
+        /// <summary>
+        /// Уточняет хочет ли закрыть
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string msg = "Do you want to close without saving?";
+            MessageBoxResult result = MessageBox.Show(msg, "My app", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        /// <summary>
+        /// Если закрыл вывод прощания
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainWindow_Closed(object? sender, EventArgs e)
+        {
+            MessageBox.Show("See ya!");
+        }
+
+        /// <summary>
+        /// Клик по кнопке и вывод сообщения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)Application.Current.Properties["GodMode"])
+            {
+                MessageBox.Show("Cheater!");
+            }
+        }
+        /// <summary>
+        /// Показывает текущие позицию курсора
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Title = e.GetPosition(this).ToString();
+        }
+    }
+}
---
+++ VisualLayoutTester/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VisualLayoutTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        protected void FileExit_Click(object sender, RoutedEventArgs args)
+        {
+            Close();
+        }
+
+        protected void ToolsSpellingHints_Click(object sender, RoutedEventArgs args) 
+        {
+            var spellingHints = string.Empty;
+            var error = txtData.GetSpellingError(txtData.CaretIndex);
+
+            if (error != null)
+            {
+                foreach (var hint in error.Suggestions)
+                {
+                    spellingHints += $"{hint}";
+                }
+
+                lblSpellingHints.Content = spellingHints;
+                expanderSpellings.IsExpanded = true;
+            }
+        }
+        protected void MouseEnterExitArea(object sender, RoutedEventArgs args) => statBarText.Text = "Exit the Aplication";
+        protected void MouseEnterToolsHintsArea(object sender, RoutedEventArgs args) => statBarText.Text = "Show Spelling Suggestions";
+        protected void MouseLeaveArea(object sender, RoutedEventArgs args) => statBarText.Text = "Ready";
+    }
+}
