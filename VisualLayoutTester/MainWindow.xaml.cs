2892fc4aa72d9ca1f56341bb60247cdbaf1a5fab
kitdim <kitaykin9808@gmail.com>
2022-11-24 21:40:43 +0300

    Добавил сохранение и открытие файла

--- VisualLayoutTester/MainWindow.xaml.cs
+++ VisualLayoutTester/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace VisualLayoutTester
 {
@@ -23,7 +25,26 @@ namespace VisualLayoutTester
         public MainWindow()
         {
             InitializeComponent();
+            SetF1CommandBinding();
         }
+
+        private void SetF1CommandBinding()
+        {
+            var helpBinding = new CommandBinding(ApplicationCommands.Help);
+            helpBinding.CanExecute += CanHelpExecute;
+            helpBinding.Executed += HelpExecuted;
+            CommandBindings.Add(helpBinding);
+        }
+        private void CanHelpExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void HelpExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Look, it is not that difficult. Just type somethings!", "Help!");
+        }
+
         protected void FileExit_Click(object sender, RoutedEventArgs args)
         {
             Close();
@@ -48,5 +69,26 @@ namespace VisualLayoutTester
         protected void MouseEnterExitArea(object sender, RoutedEventArgs args) => statBarText.Text = "Exit the Aplication";
         protected void MouseEnterToolsHintsArea(object sender, RoutedEventArgs args) => statBarText.Text = "Show Spelling Suggestions";
         protected void MouseLeaveArea(object sender, RoutedEventArgs args) => statBarText.Text = "Ready";
+        private void OpenCmdCanExecuted(object sender, CanExecuteRoutedEventArgs e) => e.CanExecute = true;
+        private void SaveCmdCanExecuted(object sender, CanExecuteRoutedEventArgs e) => e.CanExecute = true;
+
+        private void OpenCmdExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var openDlg = new OpenFileDialog { Filter = "Texr Files |*.txt" };
+            if (true == openDlg.ShowDialog())
+            {
+                string dataFromFile = File.ReadAllText(openDlg.FileName);
+                txtData.Text = dataFromFile;
+            }
+        }
+        private void SaveCmdExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var saveDlg = new OpenFileDialog { Filter = "Texr Files |*.txt" };
+            if (true == saveDlg.ShowDialog())
+            {
+                File.WriteAllText(saveDlg.FileName, txtData.Text);
+            }
+        }
+
     }
 }
